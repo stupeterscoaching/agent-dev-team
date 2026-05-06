85e0c036f89ffb3be970ed0bc20eaf63660acfe9
stupeterscoaching <stupeters1877@gmail.com>
2026-05-06 09:03:55 -0400

    [coder-17] add app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.get('/add', (req, res) => {
+    const { num1, num2 } = req.body;
+    const result = num1 + num2;
+    res.send({ result });
+});
+app.get('/subtract', (req, res) => {
+    const { num1, num2 } = req.body;
+    const result = num1 - num2;
+    res.send({ result });
+});
+app.get('/multiply', (req, res) => {
+    const { num1, num2 } = req.body;
+    const result = num1 * num2;
+    res.send({ result });
+});
+app.get('/divide', (req, res) => {
+    const { num1, num2 } = req.body;
+    if(num2 === 0) return res.status(400).send({ error: 'Cannot divide by zero' });
+    const result = num1 / num2;
+    res.send({ result });
+});
+app.use((req, res) => {
+    res.status(404).send({ message: 'Not found' });
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
